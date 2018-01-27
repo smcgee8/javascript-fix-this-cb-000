71c75ea47e4c43bed6768016f54e4a4f8e0987da
Sean McGee <sean.a.mcgee@gmail.com>
2018-01-27 03:20:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function makeCake() {
 }
 
 function makePie() {
-  pie.decorate = cake.decorate.call(pie)
+  pie.decorate = cake.decorate.call(pie);
   var updatePieStatus;
   mix(updatePieStatus)
 }
